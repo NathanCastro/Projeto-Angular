46a922340d665c4aba3568a0c4998dd34c3c1345
Nathan Castro <nathanslc18@gmail.com>
2021-04-08 11:18:29 -0300

    reorganizando componentes com a shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador/contador.component';
-import { DataBindingComponent } from './data-binding/data-binding.component';
-import { DiretivasComponent } from './diretivas/diretivas.component';
-import { ExercicioNgclassComponent } from './exercicio-ngclass/exercicio-ngclass.component';
-import { ExercicioPipesComponent } from './exercicio-pipes/exercicio-pipes.component';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { MainComponent } from './main/main.component';
+import { ContadorComponent } from './shared/components/contador/contador.component';
+import { DataBindingComponent } from './shared/components/data-binding/data-binding.component';
+import { DiretivasComponent } from './shared/components/diretivas/diretivas.component';
+import { ExercicioNgclassComponent } from './shared/components/exercicio-ngclass/exercicio-ngclass.component';
+import { ExercicioPipesComponent } from './shared/components/exercicio-pipes/exercicio-pipes.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { MainComponent } from './shared/components/main/main.component';
 
 registerLocaleData(localePT, 'pt');
 
